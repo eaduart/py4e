c95af7f4aaee345ea428f72c91d900dabb15a420
Edward Duarte <eaduart@yahoo.com>
2020-05-04 15:59:35 -0700

    Did a lot of add regarding object oriented method and techniques for party_animal.py

--- party_animal.py
+++ party_animal.py
@@ -2,13 +2,20 @@
 '''This is a part of the example for the Using Databases with Python in
    Python for Everybody certification.'''
 
+from random import choice
 
 class PartyAnimal:
     '''The party_animal class.'''
     attendees = 0
+    name = ""
 
-    def __init__(self):
+    def __init__(self, name):
         self.attendees = 0
+        self.name = name
+        print(f'{self.name} says \'I am alive\' {id(self)}')
+
+    def __del__(self):
+        print(f'{id(self)} {self.name} says \'I\'m melting away\'')
 
     def howmany(self):
         '''How many party animals are attending.'''
@@ -20,8 +27,21 @@ class PartyAnimal:
         print(f"So far {self.attendees}")
 
 
-AN = PartyAnimal()
+names = ['Fred', 'Wilma', 'Barney', 'Betty', 'Bambam', 'Pebbles', 'Dino' ]
+
+AN = PartyAnimal('Dummy')
 
 AN.party()
 AN.party()
 AN.party()
+
+print(f"Type: {type(AN)}")
+print(f"DIR: {dir(AN)}")
+
+AN = 42
+print(f'AN contians {AN}')
+
+ANlst = []
+[ ANlst.append(PartyAnimal(choice(names))) for x in range(10) ]
+
+[ print(ANlst[i].name) for i in range(len(ANlst)) ]
